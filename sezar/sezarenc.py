57c44853b2c369989b95b8aa7cbf284e9861a412
Mehmet Doğukan Aksoy <134022669+Letsrq@users.noreply.github.com>
2024-04-10 02:59:47 +0300

    hafıza ve düzen

--- sezar/sezarenc.py
+++ sezar/sezarenc.py
@@ -1,26 +1,31 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+kriptler = []
+dekriptler = []
 
 def caesar_cipher(text, shift):
     encrypted_text = ""
     for char in text:
         if char.isalpha():
             shifted = ord(char) + shift
-            if char.islower():
-                if shifted > ord('z'):
-                    shifted -= 29
-                elif shifted < ord('a'):
-                    shifted += 29
-            elif char.isupper():
-                if shifted > ord('Z'):
-                    shifted -= 29
-                elif shifted < ord('A'):
-                    shifted += 29
+            shifted = shifted if shifted < ord("z") else ord("a") + ((shifted - ord("a")) % 26)
             encrypted_text += chr(shifted)
         else:
             encrypted_text += char
     return encrypted_text
+
+def caesar_decipher(text, shift):
+    decrypted_text = ""
+    for char in text:
+        if char.isalpha():
+            shifted = ord(char) - shift  # Subtract shift to move backwards
+            shifted = shifted if shifted >= ord("a") else ord("z") - ((ord("a") - shifted - 1) % 26)
+            decrypted_text += chr(shifted)
+        else:
+            decrypted_text += char
+    return decrypted_text
 
 @app.route('/')
 def index():
@@ -31,9 +36,33 @@ def encrypt():
     text = request.form.get('text')
     shift = int(request.form.get('shift', 1)) 
     if not text:
-        return "Hic bisi yazmadin", 400
+        return "You didn't enter anything", 400
     encrypted_text = caesar_cipher(text, shift)
+    kriptler.append({'Kripto': encrypted_text, 'metin': text})
     return encrypted_text
+
+@app.route('/decrypt', methods=['POST'])
+def decrypt():
+    text = request.form.get('text')
+    shift = int(request.form.get('shift', 1)) 
+    if not text:
+        return "You didn't enter anything", 400
+    decrypted_text = caesar_decipher(text, shift)
+    dekriptler.append({'Kriptolu': text, 'Çözülmüş hali': decrypted_text})
+    return decrypted_text
+
+@app.route('/kriptler', methods=['GET'])
+def get_kriptler():
+    return jsonify(kriptler)
+
+@app.route('/dekriptler', methods=['GET'])
+def get_dekriptler():
+    return jsonify(dekriptler)
+
+@app.route('/hafiza', methods=['GET'])
+def get_hafiza():
+    hafiza = {'kriptler': kriptler, 'dekriptler': dekriptler}
+    return jsonify(hafiza)
 
 if __name__ == '__main__':
     app.run(debug=True)
